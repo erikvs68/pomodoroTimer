5fd4bfc9ea0b3f14bfc69428443c3f5415b8d6d8
raxsonic <service@raxsonic.com>
2021-09-14 12:38:36 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// You got this! 💫
+const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+let workBtn = document.getElementById('work-btn');
+let shortBtn = document.getElementById('short-break-btn');
+let longBtn = document.getElementById('long-break-btn');
+
+// 💻 Work: 25 mins
+
+function work() {
+	// click start/work button to start the 25 minute countdown
+	//when countdown gets to zero select short break or long break
+}
+// ☕️ Short break: 5 mins
+
+function shortBreak() {}
+// 🌯 Long break: 15 mins
+
+function longBreak() {}
